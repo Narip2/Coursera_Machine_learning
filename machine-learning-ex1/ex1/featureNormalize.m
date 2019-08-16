ce0dbc6805a5ffb682160de2b1f517d772edb1e9
narip <502648055@qq.com>
2019-08-17 00:05:29 +0800

    modified:   ex1/computeCostMulti.m 	modified:   ex1/ex1_multi.m 	modified:   ex1/featureNormalize.m 	modified:   ex1/gradientDescentMulti.m

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -25,14 +25,9 @@ sigma = zeros(1, size(X, 2));
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
-
-
-
-
-
-
-
-
+mu = [mean(X(:,1)) mean(X(:,2))];
+sigma = [std(X(:,1)) std(X(:,2))];
+X_norm = (X - mu)./sigma;
 
 % ============================================================
 
